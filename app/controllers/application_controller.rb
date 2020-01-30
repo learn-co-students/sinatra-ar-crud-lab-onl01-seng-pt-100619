5825900b2ee95572a08a1bb1bda518fb6de8b246
allison cortez <allizetorc@gmail.com>
2020-01-30 19:41:26 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,5 +49,4 @@ class ApplicationController < Sinatra::Base
   end
   
   
-
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,20 +1,5 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
-  # attr_accessor :name, :power, :bio
-
-  # def self.all
-  #   @@all ||= []
-  # end
-
-  # def initialize(opts={})
-  #   @name  = opts[:name]
-  #   @power = opts[:power]
-  #   @bio   = opts[:bio]
-  #   self.save
-  # end
-
-  # def save
-  #   self.class.all << self
-  # end
+ 
 end
